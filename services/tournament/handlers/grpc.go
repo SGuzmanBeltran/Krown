56479e29ba499867b653821df87fe0535dbf4647
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2024-05-21 22:04:26 -0500

    grpc tournament handler

---
+++ services/tournament/handlers/grpc.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"context"
+	proto_tournament "krown/services/genproto/tournament"
+	"krown/services/tournament/types"
+
+	"google.golang.org/grpc"
+)
+
+type TournamentGrpcHandler struct {
+	tournamentService types.TournamentService
+	proto_tournament.UnimplementedTournamentServiceServer
+}
+
+func NewGrpcTournamentService(grpc *grpc.Server, tournamentService types.TournamentService) {
+	gRpcHadler := &TournamentGrpcHandler{tournamentService: tournamentService}
+	proto_tournament.RegisterTournamentServiceServer(grpc, gRpcHadler)
+}
+
+func (h *TournamentGrpcHandler) GetTournaments(ctx context.Context, req *proto_tournament.GetTournamentsReq) (*proto_tournament.GetTournamentsRes, error) {
+	tournaments, err := h.tournamentService.GetTournaments(ctx, req)
+	if (err != nil) {
+		return nil, err
+	}
+	return tournaments, nil
+}
+
+func (h *TournamentGrpcHandler) GetTournament(ctx context.Context, req *proto_tournament.GetTournamentReq) (*proto_tournament.GetTournamentRes, error) {
+	tournament, err := h.tournamentService.GetTournament(ctx, req)
+	if (err != nil) {
+		return nil, err
+	}
+	return tournament, nil
+}
