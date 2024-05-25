182fa57c6f0363c0c5a3ef9bbbdf77cb7dae9ce5
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2024-05-24 22:13:47 -0500

    Improve Queries and datatable

--- db/migrations/20240519230248_create_tournament_table.sql
+++ db/migrations/20240519230248_create_tournament_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE tournaments (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    entry_fee INT NOT NULL,
+    entry_fee BIGINT NOT NULL,
     start_time TIMESTAMP NOT NULL
 );
 
--- db/queries/tournamentQueries.sql
+++ db/queries/tournamentQueries.sql
@@ -1,5 +1,14 @@
 -- name: GetTournaments :many
-SELECT * FROM tournaments WHERE start_time > $1 and start_time < $2;
+SELECT * FROM tournaments WHERE start_time >= $1 and start_time <= $2;
 
 -- name: GetTournament :one
 SELECT * FROM tournaments WHERE id = $1;
+
+-- name: BatchCreate :batchone
+INSERT INTO tournaments (
+    name,
+    entry_fee,
+    start_time
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
