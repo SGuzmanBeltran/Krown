d46cfa68e87d9292582a0d3dfeb9a5943396a479
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2024-05-27 20:31:02 -0500

    Add if is finished for tournaments

--- db/migrations/20240519230248_create_tournament_table.sql
+++ db/migrations/20240519230248_create_tournament_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE tournaments (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     entry_fee BIGINT NOT NULL,
-    start_time TIMESTAMP NOT NULL
+    start_time TIMESTAMP NOT NULL,
+    is_finished BOOLEAN DEFAULT FALSE
 );
 
 -- +goose Down
