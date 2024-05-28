cd67de880fdbe5fc09c4374cf80a1effb5ec1696
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2024-05-27 20:31:25 -0500

    Add column for team count users

--- db/migrations/20240505190751_create_user_table.sql
+++ db/migrations/20240505190751_create_user_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     username text NOT NULL,
     email text NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
+    teams_count INT DEFAULT 0
 );
 
 -- +goose Down
